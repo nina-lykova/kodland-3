b12b809d99dfcbd2a5823ca928601f20cfe5d0c3
nina-lykova <lykovanina38@gmail.com>
2024-09-08 13:25:33 +0300

    Add files via upload

---
+++ main_1.py
@@ -0,0 +1,34 @@
+import discord
+import random
+from discord.ext import commands
+# Переменная intents - хранит привилегии бота
+intents = discord.Intents.default()
+# Включаем привелегию на чтение сообщений
+intents.message_content = True
+# Создаем бота в переменной client и передаем все привелегии
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def number(ctx, a: int = 0, b: int = 10):
+    await ctx.send(random.randint(a, b))
+
+@bot.command()
+async def long(ctx, count_long = 10):
+    await ctx.send(random.choice("+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890") * count_long)
+@bot.command()
+async def emojis(ctx):
+    await ctx.send(random.choice('☺ ☻ ☹ シ ツ ʕʘ‿ಠʔ ￣ヘ¯ ◔◡‿◡◔ ⊙_ರೃ (͡๏̯͡๏)'))
+
+bot.run("MTI3NzI2Mjc5MzQyMjE0NzY2Nw.GQ79i-.oD68tKaXZ5VmDYI_5-Yi8ZeklEs6dg32txiHkU")
