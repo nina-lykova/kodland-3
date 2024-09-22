c92df7d34a831065ff4c944b892deb11369d140e
nina-lykova <lykovanina38@gmail.com>
2024-09-22 15:19:41 +0300

    Update main_1.py

--- main_1.py
+++ main_1.py
@@ -1,5 +1,6 @@
 import discord
 import random
+import os
 from discord.ext import commands
 # Переменная intents - хранит привилегии бота
 intents = discord.Intents.default()
@@ -9,6 +10,16 @@ intents.message_content = True
 bot = commands.Bot(command_prefix='$', intents=intents)
 
 fact = ['Более 99% населения мира дышат воздухом, который превышает пределы допустимых стандартов ВОЗ.', 'Сильный шум может оказывать негативное влияние на здоровье человека, особенно на слух.', 'К 2050 году в океане будет больше пластика, чем рыбы по весу.']
+
+help =  {
+    "$hello": "Приветсвует пользователя",
+    "$heh": "Присылает прикольную фразу",
+    "$number": "Рандомное число от 0 до 10 ",
+    "$long": "Состовляет логин длиной в 10 символов",
+    '$emojis': "Рандомный смайлик",
+    "$mem": "Рандомный мем про IT",
+    '$facts': "Интересный факт про экологию"
+}
 
 @bot.event
 async def on_ready():
@@ -32,9 +43,20 @@ async def long(ctx, count_long = 10):
 @bot.command()
 async def emojis(ctx):
     await ctx.send(random.choice('☺ ☻ ☹ シ ツ ʕʘ‿ಠʔ ￣ヘ¯ ◔◡‿◡◔ ⊙_ರೃ (͡๏̯͡๏)'))
+@bot.command()
+async def mem(ctx):
+    list_of_files = os.listdir('memes')
+    with open('memes/' + random.choice(list_of_files),'rb') as f:
+        pic = discord.File(f)
+
+    await ctx.send('Вот такой мем', file=pic)
 
 @bot.command()
 async def facts(ctx):
     await ctx.send(random.choice(fact))
+
+@bot.command()
+async def helps(ctx):
+    await ctx.send(help)
 
 bot.run("-----")
